6554e48c4abdef0994b7a2961fda3af806b16c83
rashofner <r.a.shofner@wustl.edu>
2024-10-28 23:06:53 -0500

    Code fixed and Part 3 complete

--- ICE3.py
+++ ICE3.py
@@ -1,14 +1,13 @@
-import pandas as pd
 import re
+import csv
+
 
 # Importing the .txt file named PA1_training_text.txt
-
-# help me, it didn't run
-
 def read_file(file_path):
     with open(file_path, 'r', encoding='utf-8', errors='ignore') as file:
         data = file.read()
     return data
+
 
 file_path_1 = 'PA1_training_text.txt'
 file_path_2 = 'PA2_training_text.txt'
@@ -25,44 +24,71 @@ print(f"len(split_content_1): ", len(split_content_1))
 split_content_2 = file_content_2.split()
 print(f"len(split_content_2): ", len(split_content_2))
 
-# words_over_4_1 = list()
-# words_over_4_2 = list()
-# for i in split_content_1:
-#     if len(i) >= 4:
-#         words_over_4_1.append(i)
-
-# for i in split_content_2:
-#     if len(i) < 4:
-#         words_over_4_2.append(i)
-
-# print(f"Words in PA1 (after removing): ", len(words_over_4_1))
-# print(f"Words in PA2 (after removing): ", len(words_over_4_2))
-
-
-
-
-
-
-
-# unique_words_2 = set(words_over_4_2)
-# print(f"unique words in PA2 (after removing): ", len(unique_words_2))
-
+unique_words_1 = set(split_content_1)
+print(f"unique words in PA1: ", len(unique_words_1))
+unique_words_2 = set(split_content_2)
+print(f"unique words in PA2: ", len(unique_words_2))
 
 removed_words_1 = [ele for ele in split_content_1 if len(ele) > 3]
 print("PA1 Words list (after removing):", len(removed_words_1))
-unique_words_1 = set(removed_words_1)
-print(f"unique words in PA1 (after removing): ", len(unique_words_1))
+removed_unique_words_1 = set(removed_words_1)
+print(f"unique words in PA1 (after removing): ", len(removed_unique_words_1))
 
 removed_words_2 = [ele for ele in split_content_2 if len(ele) > 3]
 print("PA2 Words list (after removing):", len(removed_words_2))
-unique_words_2 = set(removed_words_2)
-print(f"unique words in PA2 (after removing): ", len(unique_words_2))
+removed_unique_words_2 = set(removed_words_2)
+print(f"unique words in PA2 (after removing): ", len(removed_unique_words_2))
 
-vocabulary = unique_words_1.union(unique_words_2)
+vocabulary = removed_unique_words_1.union(removed_unique_words_2)
 print(f"Vocabulary length: ", len(vocabulary))
+# Sort the vocabulary alphabetically
+sorted_vocabulary = sorted(vocabulary)
 
+
+# calculate word counts for PA1 and PA2
 PA1_dict = dict()
 PA2_dict = dict()
+
+for word in sorted_vocabulary:
+    PA1_dict[word] = removed_words_1.count(word)
+    PA2_dict[word] = removed_words_2.count(word)
+
+
+v = len(vocabulary)
+n1 = len(removed_words_1)
+n2 = len(removed_words_2)
+
+# Calculate conditional probabilities with Laplace smoothing
+PA1_probabilities = dict()
+PA2_probabilities = dict()
+
+# calculate probabilities
+for word in sorted_vocabulary:
+    PA1_probabilities[word] = (PA1_dict[word] + 1) / (n1 + v)
+    PA2_probabilities[word] = (PA2_dict[word] + 1) / (n2 + v)
+
+
+# paths for saving to csv
+PA1_csv = 'PA1_probabilities.csv'
+PA2_csv = 'PA2_probabilities.csv'
+
+# save PA1 and PA2 probabilities to csv
+with open(PA1_csv, mode='w', newline='') as file:
+    writer = csv.writer(file)
+    # Write header
+    writer.writerow(['Vocabulary', 'P(word|PA1)'])
+    # Write each word and its probability
+    for word in sorted_vocabulary:
+        writer.writerow([word, PA1_probabilities[word]])
+
+with open(PA2_csv, mode='w', newline='') as file:
+    writer = csv.writer(file)
+    # headers
+    writer.writerow(['Vocabulary', 'P(word|PA2)'])
+    # word and probability
+    for word in sorted_vocabulary:
+        writer.writerow([word, PA2_probabilities[word]])
+
 
 for i in vocabulary:
     PA1_dict[i] = 0
@@ -70,11 +96,11 @@ for i in vocabulary:
         if i == j:
             PA1_dict[i] += 1
 
-
 for i in vocabulary:
     PA2_dict[i] = 0
     for j in removed_words_2:
         if i == j:
             PA2_dict[i] += 1
 
-print(PA1_dict["algorithm"])
+print("Number of times 'algorithm' is in PA1: ", PA1_dict["algorithm"])
+print("Number of times 'algorithm' is in PA2: ", PA2_dict["algorithm"])
--- README.md
+++ README.md
@@ -43,3 +43,58 @@
 #### Q1.9
 
 - 0
+
+
+#### Q2.1
+
+- 0.5
+- 0.5
+
+
+#### Q2.2
+
+- will not
+- can ignore them
+
+
+#### Q2.3
+
+- the number of times the word algorithm appears in PA1
+- the number of words in PA1
+
+
+#### Q2.4
+
+- 1
+- v
+
+
+
+#### Q3.1
+
+- apple = 1, banana = 0, wheel = 2, engine = 0
+
+
+#### Q3.2
+
+- 0.003
+
+
+#### Q3.3
+
+- 0.008
+
+
+#### Q3.4
+
+- class B
+
+
+
+
+
+
+
+
+
+
