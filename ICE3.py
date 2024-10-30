6e2f6b067ad3ccf1e8c49030c82b21a0415d0257
nick-cochran <c.nick@wustl.edu>
2024-10-30 13:16:11 -0500

    finished up and added to readme

--- ICE3.py
+++ ICE3.py
@@ -203,3 +203,56 @@ for test_name, test_words in test_files.items():
     print(f"Proportional probability of PA1 for {test_name}: {PA1_test_prob}")
     print(f"Proportional probability of PA2 for {test_name}: {PA2_test_prob}")
     print()
+
+
+# Question 5
+
+file_path_1 = 'PA2_testing_text_1.txt'
+file_path_2 = 'PA2_testing_text_2.txt'
+file_path_3 = 'PA2_testing_text_3.txt'
+file_path_4 = 'PA2_testing_text_4.txt'
+file_content_1 = read_file(file_path_1)
+file_content_2 = read_file(file_path_2)
+file_content_3 = read_file(file_path_3)
+file_content_4 = read_file(file_path_4)
+
+comb_file_contents = file_content_1 + file_content_2 + file_content_3 + file_content_4
+
+comb_file_contents = comb_file_contents.lower()
+comb_file_contents = re.sub(r'[.,:]', '', comb_file_contents)
+
+print("Combined test data")
+split_content = comb_file_contents.split()
+print(f"len(split_content_1): ", len(split_content))
+
+unique_words = set(split_content)
+print(f"unique words in PA1: ", len(unique_words))
+
+removed_words = [ele for ele in split_content if len(ele) > 3]
+print("PA1 Words list (after removing):", len(removed_words))
+removed_unique_words = set(removed_words)
+print(f"unique words in PA1 (after removing): ", len(removed_unique_words))
+
+# create bag of words for test set
+test_counts = {word: removed_words.count(word) for word in sorted_vocabulary}
+
+PA1_test_prob = 1
+PA2_test_prob = 1
+PA1_bad_muls_count = 0
+PA2_bad_muls_count = 0
+
+# calculate approximate probabilities
+for word, count in test_counts.items():
+    if count > 0:
+        if PA1_test_prob * PA1_probabilities[word] ** count != 0.0:
+            PA1_test_prob *= PA1_probabilities[word] ** count
+        else:
+            PA1_bad_muls_count += 1
+        if PA2_test_prob * PA2_probabilities[word] ** count != 0.0:
+            PA2_test_prob *= PA2_probabilities[word] ** count
+        else:
+            PA2_bad_muls_count += 1
+        # print(f"PA1_prob: {PA1_probabilities[word]}, PA2_prob: {PA2_probabilities[word]}, PA1_test_prob: {PA1_test_prob}, PA2_test_prob: {PA2_test_prob}")
+
+print(f"Proportional probability of PA1 for combined test data: {PA1_test_prob} with {PA1_bad_muls_count} multiplications that would've resulted in 0.0")
+print(f"Proportional probability of PA2 for combined test data: {PA2_test_prob} with {PA2_bad_muls_count} multiplications that would've resulted in 0.0")
--- README.md
+++ README.md
@@ -89,11 +89,36 @@
 
 - class B
 
+#### Q4.3
 
+Proportional probability of PA1 for PA2_test_1: 2.952445311338666e-60
+Proportional probability of PA2 for PA2_test_1: 1.7389994927401387e-54
+
+Proportional probability of PA1 for PA2_test_2: 1.2889321368699806e-110
+Proportional probability of PA2 for PA2_test_2: 5.424100708836807e-102
+
+Proportional probability of PA1 for PA2_test_3: 2.980789830276087e-111
+Proportional probability of PA2 for PA2_test_3: 2.3994485823667224e-102
+
+Proportional probability of PA1 for PA2_test_4: 3.55711346361056e-103
+Proportional probability of PA2 for PA2_test_4: 5.603753997340804e-97
+
+This correctly predicts that PA2_test 1-4 are all in PA2.
 
 #### Q5.1
 
 - 312
+
+#### Q5.2
+
+Proportional probability of PA1 for combined test data: 5e-324 with 5 multiplications that would've resulted in 0.0
+Proportional probability of PA2 for combined test data: 1.874640894e-315 with 2 multiplications that would've resulted in 0.0
+
+It appears from these numbers that it is more likely to be from PA2.  We were getting a result of 0.0, but we made adjustments to preserve the number before it would've become 0 and then kept a count of how many more multiplications needed to happen after it would've turned to 0.
+
+#### Q5.3
+
+Adding logarithms of a number is proportional to multiplying those numbers because the result of a logarithm is the exponent that would get the provided number, and adding exponents is the same as multiplying the bases.  Therefore, adding the logarithms of a number is proportional to multiplying those numbers.
 
 
 
